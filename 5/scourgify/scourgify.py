8da97781725025aa5dedf2b2adcc413b13740dc4
Eugenio Soria <eugeniosoria@gmail.com>
2024-12-20 08:28:29 -0300

    scourgify

--- 5/scourgify/scourgify.py
+++ 5/scourgify/scourgify.py
@@ -1,8 +1,8 @@
 import sys, csv
 
-def invertir_nombre(apellido_nombre):
-    nombre, apellido = map(str.strip, apellido_nombre.split(","))
-    return f"{nombre}, {apellido}"
+def invertir_nombre(last_first):
+    last, first = map(str.strip, last_first.split(","))
+    return first, last
 
 
 if len(sys.argv) < 3:
@@ -18,10 +18,28 @@ except:
 if e != "csv":    
     sys.exit("Not a CSV file")
     
+    
 else:
-    with open(sys.argv[1]) as file:
-        for row in file:
-            row[0] =
+    with open(sys.argv[1], newline='') as before, open("after.csv", "w") as after:
+        reader = csv.DictReader(before)
+        writer = csv.DictWriter(after, fieldnames=["first", "last", "house"])
+        writer.writeheader()
+        #rows_cvs = []
+        for row in reader:
+            first, last = invertir_nombre(row['name'])
+            house = row['house']
+            writer.writerow(
+                {
+                    "first" : first,
+                    "last" : last,
+                    "house" : house
+                }
+            )
+
+
+    
+
+            
         
         
 
